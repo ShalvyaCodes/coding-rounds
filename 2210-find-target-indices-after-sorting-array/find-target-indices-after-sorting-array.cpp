a527cf3db82714e4a00b76be7df1d3392dbd7403
ShalvyaCodes <shalvya0408@gmail.com>
2026-09-17 00:02:33 +0530

    Time: 4 ms (3.90%) | Memory: 15.1 MB (52.13%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    std::vector<int> targetIndices(std::vector<int>& nums, int target) {
+        
+        std::sort(nums.begin(), nums.end());
+        
+        std::vector<int> result;
+        
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == target) {
+                result.push_back(i);
+            }
+        }
+        
+        return result;
+    }
+};
