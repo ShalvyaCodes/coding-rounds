00e06c5ba79967d6773c0421a1ae5c48ed0c3161
ShalvyaCodes <shalvya0408@gmail.com>
2026-09-23 00:14:42 +0530

    Time: 4 ms (40.56%) | Memory: 71.4 MB (20.03%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+class Solution {
+public:
+    int candy(std::vector<int>& ratings) {
+        int n = ratings.size();
+        std::vector<int> candies(n, 1);
+        
+        // Left-to-right pass
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        
+        // Right-to-left pass
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = std::max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        
+        // Sum up the total number of candies
+        return std::accumulate(candies.begin(), candies.end(), 0);
+    }
+};
